de250cb3967b0485da73c973362b8d6ad6b03809
Nicolas Jacquemin <nicolas1.jacquemin@epitech.eu>
2025-10-17 17:23:45 +0300

    disable web middleware on oauth routes

--- routes/oauth.php
+++ routes/oauth.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\OAuth\AccessTokenController;
 use App\Http\Controllers\OAuth\AuthorizationController;
 use App\Http\Controllers\OAuth\LogoutController;
-use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/authorize', [AuthorizationController::class, 'authorize'])
@@ -11,7 +10,7 @@ Route::get('/authorize', [AuthorizationController::class, 'authorize'])
     ->name('passport.authorizations.authorize');
 
 Route::post('/token', [AccessTokenController::class, 'issueToken'])
-    ->withoutMiddleware([VerifyCsrfToken::class])
+    ->withoutMiddleware('web')
     ->name('passport.token');
 
 Route::get('logout', LogoutController::class)
